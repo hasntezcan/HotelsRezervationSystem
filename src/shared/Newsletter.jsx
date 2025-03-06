ce5818f23d0c158b9211134c39387ab99f17eeb7
hasntezcan <hasantezcann9@gmail.com>
2025-03-06 16:48:15 +0300

    mobile responsive, header icons

--- src/shared/Newsletter.jsx
+++ src/shared/Newsletter.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import './newsletter.css'
 import { Container, Row, Col } from 'reactstrap'
-import maleTourist from '../assets/images/male-tourist.png'
 
 const NewsLetter = () => {
    return (
