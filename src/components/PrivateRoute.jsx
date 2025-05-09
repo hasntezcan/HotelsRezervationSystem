8855f423f87b193a85031782d1a3b2fe72a3f7e2
iamsahinemir <114481847+iamsahinemir@users.noreply.github.com>
2025-05-09 19:48:12 +0300

    Url access
    Access problemi kesin çözüldü

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,12 +1,30 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
+import { useContext } from 'react';
 import { AuthContext } from '../context/AuthContext';
 
-const PrivateRoute = () => {
+const PrivateRoute = ({ allowedRoles }) => {
   const { user } = useContext(AuthContext);
-  
-  // Eğer kullanıcı giriş yapmamışsa, login sayfasına yönlendir
-  return user ? <Outlet /> : <Navigate to="/login" />;
+
+  // Henüz login olunmamışsa login sayfasına
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  }
+
+  // Rol, allowedRoles içinde değilse
+  if (!allowedRoles.includes(user.role)) {
+    // Admin kendi sayfasına, manager kendi sayfasına, diğerleri login'e yönlensin
+    const redirectTo =
+      user.role === 'admin'
+        ? '/admin'
+        : user.role === 'manager'
+        ? '/manager'
+        : '/login';
+    return <Navigate to={redirectTo} replace />;
+  }
+
+  // Yetkilendirme tamam, alt routeları (Outlet) render et
+  return <Outlet />;
 };
 
 export default PrivateRoute;
--- src/router/Routers.jsx
+++ src/router/Routers.jsx
@@ -1,78 +1,82 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import ThankYou from '../pages/ThankYou';
-import Home from '../pages/Home';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import PrivateRoute from '../components/PrivateRoute';
+
 import Login from '../pages/Login';
 import Register from '../pages/Register';
-import TourDetails from '../pages/TourDetails';
-import Hotels from '../pages/Hotels'; 
-import Manager from '../pages/Manager';
-import ManagerHotels from '../pages/ManagerHotels';  
-import ManagerReservations from '../pages/ManagerReservations';  
-import ManagerProfile from '../pages/ManagerProfile';  
-import Admin from '../pages/Admin';
-import AdminContact from '../pages/AdminContact';
-import AdminUser from '../pages/AdminUser.jsx';
-import AdminDashboard from '../pages/AdminDashboard';
+import ResetPassword from '../pages/ResetPassword';
+import Home from '../pages/Home';
 import About from '../pages/About';
 import Contact from '../pages/Contact';
-import SearchResults from "../pages/SearchResults";
-import AdminProfile from '../pages/AdminProfile';
+import Hotels from '../pages/Hotels';
+import TourDetails from '../pages/TourDetails';
+import SearchResults from '../pages/SearchResults';
+import PaymentPage from '../pages/Booking/PaymentPage';
+import ThankYou from '../pages/ThankYou';
 import Profile from '../pages/profile/Profile';
-import ProfileBookings from '../pages/profile/ProfileBookings.jsx';
-import ProfileSettings from '../pages/profile/ProfileSettings';
 import Overview from '../pages/profile/Overview';
-import AdminHotel from '../pages/AdminHotels.jsx';
-import PaymentPage from '../pages/Booking/PaymentPage.jsx';
-import PrivateRoute from '../components/PrivateRoute';
-import ManagerRoute from '../components/ManagerRoute';
-import AdminRoute from '../components/AdminRoute';
-import ResetPassword from '../pages/ResetPassword.jsx';
-import Invoice from '../components/Invoice/InvoicePage.tsx';
+import ProfileBookings from '../pages/profile/ProfileBookings';
+import ProfileSettings from '../pages/profile/ProfileSettings';
 
-const Routers = () => {
-  return (
-    <Routes>
-      {/* Public Pages */}
+import Manager from '../pages/Manager';
+import ManagerHotels from '../pages/ManagerHotels';
+import ManagerReservations from '../pages/ManagerReservations';
+import ManagerProfile from '../pages/ManagerProfile';
+
+import Admin from '../pages/Admin';
+import AdminDashboard from '../pages/AdminDashboard';
+import AdminContact from '../pages/AdminContact';
+import AdminUser from '../pages/AdminUser';
+import AdminProfile from '../pages/AdminProfile';
+import AdminHotels from '../pages/AdminHotels';
+
+const Routers = () => (
+  <Routes>
+    {/* Auth Routes */}
+    <Route path="/login" element={<Login />} />
+    <Route path="/register" element={<Register />} />
+    <Route path="/reset-password" element={<ResetPassword />} />
+
+    {/* User-only */}
+    <Route element={<PrivateRoute allowedRoles={['user']} />}>
       <Route path="/" element={<Home />} />
       <Route path="/home" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/hotels" element={<Hotels />} />
       <Route path="/hotels/:id" element={<TourDetails />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/reset-password" element={<ResetPassword />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/thank-you" element={<ThankYou />} />
       <Route path="/search-results" element={<SearchResults />} />
       <Route path="/payment" element={<PaymentPage />} />
-      <Route path="/invoice" element={<Invoice />} />
+      <Route path="/thank-you" element={<ThankYou />} />
       <Route path="/profile" element={<Profile />}>
         <Route index element={<Overview />} />
         <Route path="bookings" element={<ProfileBookings />} />
         <Route path="settings" element={<ProfileSettings />} />
       </Route>
+    </Route>
 
-      {/* Protected Manager Routes: Yalnızca manager'lar erişebilsin */}
-      <Route element={<ManagerRoute />}>
-        <Route path="/manager" element={<Manager />} />
-        <Route path="/manager/hotel" element={<ManagerHotels />} />
-        <Route path="/manager/reservations" element={<ManagerReservations />} />
-        <Route path="/manager/profile" element={<ManagerProfile />} />
-      </Route>
+    {/* Manager-only */}
+    <Route element={<PrivateRoute allowedRoles={['manager']} />}>
+      <Route path="/manager" element={<Manager />} />
+      <Route path="/manager/hotel" element={<ManagerHotels />} />
+      <Route path="/manager/reservations" element={<ManagerReservations />} />
+      <Route path="/manager/profile" element={<ManagerProfile />} />
+    </Route>
 
-      {/* Protected Admin Routes: Yalnızca admin'ler erişebilsin */}
-      <Route element={<AdminRoute />}>
-        <Route path="/admin/*" element={<Admin />}>
-          <Route index element={<AdminDashboard />} />
-          <Route path="contactUs" element={<AdminContact />} />
-          <Route path="user" element={<AdminUser />} />
-          <Route path="profile" element={<AdminProfile />} />
-          <Route path="hotels" element={<AdminHotel />} />
-        </Route>
+    {/* Admin-only */}
+    <Route element={<PrivateRoute allowedRoles={['admin']} />}>
+      <Route path="/admin/*" element={<Admin />}>
+        <Route index element={<AdminDashboard />} />
+        <Route path="contactUs" element={<AdminContact />} />
+        <Route path="user" element={<AdminUser />} />
+        <Route path="profile" element={<AdminProfile />} />
+        <Route path="hotels" element={<AdminHotels />} />
       </Route>
-    </Routes>
-  );
-};
+    </Route>
+
+    {/* Hiçbir rota eşleşmezse login */}
+    <Route path="*" element={<Navigate to="/login" replace />} />
+  </Routes>
+);
 
 export default Routers;
